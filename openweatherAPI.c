aa4a5614ca4fdc471a30657efc2e99f09cf6da87
yulimmm <89307538+yulimmm@users.noreply.github.com>
2022-02-20 21:34:33 +0900

    Update openweatherAPI.c

--- openweatherAPI.c
+++ openweatherAPI.c
@@ -22,7 +22,7 @@ void UART1_transmit(char data)
 void UART1_string_transmit(char *str)
 {
 	for(int i = 0; str[i]; i++)    //들어오는 문자를 문자열로 저장
-	UART1_transmit(str[i]);        
+	UART1_transmit(str[i]);
 }
 
 unsigned char UART1_receive(void)
@@ -38,88 +38,104 @@ void UART1_string_receive(char *str)
 	for(i = 0; data != '\n'; i++)     //\n이 들어올 때까지의 문자를 data에 저장
 	{
 		str[i] = data;
-		data = UART1_receive();       
-	}
-}
-
-char UART2_string_receive(char *str)
-{
-	int i;
-	char data = UART1_receive();
-	for(i = 0; data != '\n'; i++)     //\n이 들어올 때까지의 문자를 data에 저장
-	{
-		str[i] = data;
 		data = UART1_receive();
 	}
-	return data;
 }
 
-void jsonParsing(char* json){
+void str_clean(char *str)
+{
+	for(int i = 0; str[i] != '\0'; i++)  //str[i] = 0이 아닌 동안
+	str[i] = '\0';       //str[i] = 0
+}
 
-	char *sArr[100] = {NULL, };
-	int i = 0;
+void readJSON(char* tmp){
+	int count = 0;
+	UART1_string_transmit("GET /data/2.5/weather?id=1835848&units=metric&APPID=b154f4f2fefe7862a10432af6e2b4dbd\r\n"); 
+	UART1_string_transmit("Host: api.openweathermap.org\r\n");
+	UART1_string_transmit("User-Agent: ArduinoWiFi/1.1\r\n");
+	UART1_string_transmit("Connection: close\r\n\r\n"); //138
 	
-	char *ptr = strtok(*json, "\"");
-	while (ptr != NULL)            // " 문자열이 나오지 않을 때까지 반복
-	{
-		sArr[i] = ptr;             // 문자열을 자른 뒤 메모리 주소를 문자열 포인터 배열에 저장
-		i++;                       // 인덱스 증가
-
-		ptr = strtok(NULL, "\"");   // 다음 문자열을 잘라서 포인터를 반환
+	char data = UART1_receive();
+	int i = 0;
+	while(i < 1000){
+		if(i >= 4){
+			tmp[0] = tmp[1];
+			tmp[1] = tmp[2];
+			tmp[2] = tmp[3];
+			tmp[3] = data;
+			data = UART1_receive();
+		}
+		else{
+			tmp[i] = data;
+			data = UART1_receive();
+		}
+		if(tmp[0] == 'm' && tmp[1] == 'a' && tmp[2] == 'i' && tmp[3] == 'n'){
+			while(1)
+			{
+				count++;
+				tmp[0] = tmp[1];
+				tmp[1] = tmp[2];
+				tmp[2] = tmp[3];
+				tmp[3] = data;
+				if(count == 10)	return;
+				data = UART1_receive();
+				i++;
+				
+			}
+		}
+		i++;
 	}
-
-	for (int i = 0; i < 10; i++)
-	{
-		if (sArr[i] != NULL)           // 문자열 포인터 배열의 요소가 NULL이 아닐 때만
-		printf("%s\n", sArr[i]);   // 문자열 포인터 배열에 인덱스로 접근하여 각 문자열 출력
-	}
-
 }
 
 int main(void)
 {
+	DDRB = 0xFF; // 0b11111111
 	char temp[800];
-//	char weatherdata[800];
- 	UART1_init();
-	 
-// 	UART1_string_transmit("AT+RST\r\n"); //reset
-// 	UART1_string_receive(temp);
-// 	_delay_ms(100);
+	char weather[5];
 	
-// 	UART1_string_transmit("ATE0\r\n");  //echo off
-// 	_delay_ms(100);
-// 	UART1_string_receive(temp);
+	UART1_init();
 	
- 	UART1_string_transmit("AT+CWMODE=1\r\n");  //wifi mode 설정
-	UART1_string_receive(temp);
-	_delay_ms(5000);
- 	
- 	UART1_string_transmit("AT+CWJAP=\"TP-Link_B9D0\",\"06312275\"\r\n");  //wifi 연결
-	UART1_string_receive(temp);
-	_delay_ms(5000);
- 	
- 	UART1_string_transmit("AT+CIPSTART=\"TCP\",\"api.openweathermap.org\",80\r\n");   // api 요청, 형식:<type><remoteIP><remote port>
- 	UART1_string_receive(temp);
-	 _delay_ms(5000);
-	 
- 	UART1_string_transmit("AT+CIPSEND=138\r\n"); //보내는 문자 길이 133
-	UART1_string_receive(temp);
-	_delay_ms(5000);
- 	
-	UART1_string_transmit("GET /data/2.5/weather?id=1835848&units=metric&APPID=b154f4f2fefe7862a10432af6e2b4dbd\r\n"); //133
-	UART1_string_transmit("Host: api.openweathermap.org\r\n"); 
-	UART1_string_transmit("User-Agent: ArduinoWiFi/1.1\r\n");
-	UART1_string_transmit("Connection: close\r\n\r\n"); //138
-	UART1_string_receive(temp);
-	_delay_ms(5000);
- 	
-	jsonParsing(UART2_string_receive(temp));
+	// 	UART1_string_transmit("AT+RST\r\n"); //reset
+	// 	UART1_string_receive(temp);
+	// 	_delay_ms(100);
 	
+	// 	UART1_string_transmit("ATE0\r\n");  //echo off
+	// 	_delay_ms(100);
+	// 	UART1_string_receive(temp);
+	
+	UART1_string_transmit("AT+CWMODE=1\r\n");  //wifi mode 설정
+	UART1_string_receive(temp);
+	_delay_ms(5000);
+	
+	UART1_string_transmit("AT+CWJAP=\"Takeout1_2.4G\",\"takeout0503\"\r\n");
+	//UART1_string_transmit("AT+CWJAP=\"TP-Link_B9D0\",\"06312275\"\r\n");  //wifi 연결
+	UART1_string_receive(temp);
+	_delay_ms(5000);
+	
+	str_clean(weather);
+	
+	UART1_string_transmit("AT+CIPSTART=\"TCP\",\"api.openweathermap.org\",80\r\n");   // api 요청, 형식:<type><remoteIP><remote port>
+	UART1_string_receive(temp);
+	_delay_ms(5000);
+	
+	UART1_string_transmit("AT+CIPSEND=138\r\n"); //보내는 문자 길이 138
+	UART1_string_receive(temp);
+	_delay_ms(5000);
 	
 	while(1)
 	{
-// 		UART1_string_receive(temp);
-// 		_delay_ms(50);
+		readJSON(weather);
+		if((weather[0] == 'R' && weather[1] == 'a' && weather[2] == 'i' && weather[3] == 'n') || (weather[0] == 'S' && weather[1] == 'n' && weather[2] == 'o' && weather[3] == 'w') ){
+			PORTB = 0b11111111;
+			_delay_ms(200);
+			break;
+		}
+		else{
+			PORTB = 0b10101010;
+			_delay_ms(500);
+			break;
+
+		}
 	}
 	return 0;
 	
